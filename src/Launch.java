f8a2d0786fe8c9c1387bcce36c242320db10bfe7
Hassan <hassan_99@live.dk>
2022-04-19 18:09:05 +0200

    running a test

--- src/Launch.java
+++ src/Launch.java
@@ -1,6 +1,21 @@
+import hk.activity.Activity;
+import hk.location.UserLocation;
+import hk.settings.Settings;
+import hk.utility.loader.ActivityLoader;
+
 public class Launch {
 
     public static void main(String[] args) {
+
+        UserLocation userLocation = new UserLocation(Settings.DEFAULT_LATITUDE, Settings.DEFAULT_LONGITUDE);
+        userLocation.fetch();
+        userLocation.parse();
+
+        ActivityLoader activityLoader = new ActivityLoader(Settings.ACTIVITY_JSON_PATH);
+        activityLoader.init();
+
+        for (Activity activity : Activity.activities)
+            System.out.println(activity + ", distance: " + userLocation.calculateDistance(activity.getCoordinate().getLatitude(), activity.getCoordinate().getLongitude()) + " km");
 
     }
 
