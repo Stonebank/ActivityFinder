6f2b7cbcefd2dcfec754d78a198a47961f19e17a
Hassan <hassan_99@live.dk>
2022-04-19 21:05:56 +0200

    Adding path to geolocation database

--- src/hk/settings/Settings.java
+++ src/hk/settings/Settings.java
@@ -1,8 +1,12 @@
 package hk.settings;
 
+import java.io.File;
+
 public class Settings {
 
     public static final boolean DEBUG = false;
+
+    public static final File GEO_DATABASE = new File("./resources/lib/geo-lib/GeoLite2-City.mmdb");
 
     public static final String WEATHER_API_KEY = "a0a4ba68146a9c6451e13417ea962641";
     public static final String WEATHER_UNIT_OUTPUT = "metric";
