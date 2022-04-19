47e7bbaea0619314033ef7f4f5f79194e6418305
Hassan <hassan_99@live.dk>
2022-04-19 16:24:39 +0200

    Settings.java created for easier configuration of various variables

---
+++ src/hk/settings/Settings.java
@@ -0,0 +1,11 @@
+package hk.settings;
+
+public class Settings {
+
+    public static final String WEATHER_API_KEY = "a0a4ba68146a9c6451e13417ea962641";
+    public static final String WEATHER_UNIT_OUTPUT = "metric";
+
+    public static final double DEFAULT_LONGITUDE = 55.871984303779705;
+    public static final double DEFAULT_LATITUDE = 12.357251514307459;
+
+}
