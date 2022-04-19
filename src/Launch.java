52f0451b3c3ea3d1127ce0519dcb306c1b1f55ef
Hassan <hassan_99@live.dk>
2022-04-19 19:24:57 +0200

    testing the category system

--- src/Launch.java
+++ src/Launch.java
@@ -1,4 +1,6 @@
 import hk.activity.Activity;
+import hk.activity.category.Category;
+import hk.activity.category.container.BestDistance;
 import hk.location.UserLocation;
 import hk.settings.Settings;
 import hk.utility.loader.ActivityLoader;
@@ -13,6 +15,12 @@ public class Launch {
 
         ActivityLoader activityLoader = new ActivityLoader(Settings.ACTIVITY_JSON_PATH);
         activityLoader.init();
+
+        Category bestDistance = new BestDistance();
+        bestDistance.compare();
+        bestDistance.addPoints();
+        System.out.println("Best candidate: " + bestDistance.bestCandidate());
+        System.out.println();
 
         for (Activity activity : Activity.activities)
             System.out.println(activity + ", distance: " + userLocation.calculateDistance(activity.getCoordinate().getLatitude(), activity.getCoordinate().getLongitude()) + " km");
