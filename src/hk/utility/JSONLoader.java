6295d7d572719cdab349d2c4c04328f303df2f0d
Hassan <hassan_99@live.dk>
2022-04-19 18:09:52 +0200

    Removing the test method

--- src/hk/utility/JSONLoader.java
+++ src/hk/utility/JSONLoader.java
@@ -7,30 +7,6 @@ import java.nio.file.Paths;
 
 public abstract class JSONLoader {
 
-    public static void main(String[] args) {
-        JSONLoader jsonLoader = new JSONLoader() {
-            @Override
-            public String json_file() {
-                return "./resources/json/activity.json";
-            }
-
-            @Override
-            public void load(JsonObject reader, Gson gson) {
-
-                String name = reader.get("name").getAsString();
-
-                double latitude = reader.get("coordinate").getAsJsonObject().get("latitude").getAsDouble();
-                double longitude = reader.get("coordinate").getAsJsonObject().get("longitude").getAsDouble();
-
-                // parse weather types
-
-                System.out.println(name + " " + latitude + " " + longitude);
-
-            }
-        };
-        jsonLoader.load();
-    }
-
     public abstract String json_file();
 
     public abstract void load(JsonObject reader, Gson gson);
