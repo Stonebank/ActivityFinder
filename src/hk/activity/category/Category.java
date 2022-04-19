98438ba8eb9417d222721a10bf0f992617cb91c2
Hassan <hassan_99@live.dk>
2022-04-19 19:24:45 +0200

    Category.java: Abstract class, allowing to create categories to compare activities for points

---
+++ src/hk/activity/category/Category.java
@@ -0,0 +1,13 @@
+package hk.activity.category;
+
+import hk.activity.Activity;
+
+public abstract class Category {
+
+    public abstract void compare();
+
+    public abstract void addPoints();
+
+    public abstract Activity bestCandidate();
+
+}
