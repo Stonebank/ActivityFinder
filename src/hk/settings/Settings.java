c3db27b16c1e59ea7c295b56a598b24f1d31bd31
Hassan <hassan_99@live.dk>
2022-04-19 18:41:04 +0200

    Disabling debug mode

--- src/hk/settings/Settings.java
+++ src/hk/settings/Settings.java
@@ -2,7 +2,7 @@ package hk.settings;
 
 public class Settings {
 
-    public static final boolean DEBUG = true;
+    public static final boolean DEBUG = false;
 
     public static final String WEATHER_API_KEY = "a0a4ba68146a9c6451e13417ea962641";
     public static final String WEATHER_UNIT_OUTPUT = "metric";
