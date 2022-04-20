49631cd913b8417472b6b862236ddee48afe728c
Hassan <hassan_99@live.dk>
2022-04-20 04:27:28 +0200

    Adjustments to the best weather point system

--- src/hk/activity/category/container/BestWeather.java
+++ src/hk/activity/category/container/BestWeather.java
@@ -2,6 +2,7 @@ package hk.activity.category.container;
 
 import hk.activity.Activity;
 import hk.activity.category.Category;
+import hk.activity.weather.WeatherType;
 import hk.location.UserLocation;
 
 public class BestWeather extends Category {
@@ -13,9 +14,9 @@ public class BestWeather extends Category {
         userLocation.parse();
 
         for (Activity activity : Activity.activities) {
-            if (activity.getBestWeather() == userLocation.getWeatherType())
+            if (activity.getBestWeather() == userLocation.getWeatherType() || activity.getBestWeather() == WeatherType.NONE)
                 activity.addPoints(100);
-            else
+            if (activity.getWorstWeather() == userLocation.getWeatherType())
                 activity.removePoints(200);
         }
 
