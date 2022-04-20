b5942c3e84cad73250436723ef6318da980861cc
Hassan <hassan_99@live.dk>
2022-04-20 03:16:54 +0200

    Corrected default geo coordinates

--- src/hk/settings/Settings.java
+++ src/hk/settings/Settings.java
@@ -11,8 +11,8 @@ public class Settings {
     public static final String WEATHER_API_KEY = "a0a4ba68146a9c6451e13417ea962641";
     public static final String WEATHER_UNIT_OUTPUT = "metric";
 
-    public static final double DEFAULT_LONGITUDE = 55.871984303779705;
-    public static final double DEFAULT_LATITUDE = 12.357251514307459;
+    public static final double DEFAULT_LATITUDE = 55.871984303779705;
+    public static final double DEFAULT_LONGITUDE = 12.357251514307459;
 
     public static final String ACTIVITY_JSON_PATH = "./resources/json/activity.json";
 
